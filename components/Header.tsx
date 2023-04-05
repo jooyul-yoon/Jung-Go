64b937cb259cad6a7fc6daba63a65c542ff8214c
Jooyul Yoon <juyeolyoon@gmail.com>
2023-04-05 11:30:32 +0000

    Header login logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,35 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
+import { Router } from "next/router";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { tokenAtom } from "../atoms/atoms";
 
 export default function Header() {
+  const router = useRouter();
+  const jwt = localStorage.jwt ? JSON.parse(localStorage.jwt) : null;
+  const [token, setToken] = useRecoilState(tokenAtom);
+
+  const logout = () => {
+    setToken(null);
+    router.refresh();
+  };
+
   return (
     <div className={styles.header_container}>
       <Link className={styles.homeBtn} href={"/"}>
         Home
       </Link>
-      <Link href={"/login"} className={styles.title}>
-        <div>Log In</div>
-      </Link>
-      <Link href={"/signup"} className={styles.title}>
-        <div>Sign Up</div>
-      </Link>
+      {!token ? (
+        <Link href={"/login"} className={styles.title}>
+          <div>Log In</div>
+        </Link>
+      ) : (
+        <button onClick={logout}>
+          <div>Sign Out</div>
+        </button>
+      )}
     </div>
   );
 }
